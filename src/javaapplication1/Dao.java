9efe609f02eb2ae7b1b798fb731b9469fc11d3d1
AnotherCreator <jreginaldo@protonmail.com>
2022-04-26 11:58:27 -0500

    Add initial project files

---
+++ src/javaapplication1/Dao.java
@@ -0,0 +1,146 @@
+package javaapplication1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Dao {
+	// instance fields
+	static Connection connect = null;
+	Statement statement = null;
+
+	// constructor
+	public Dao() {
+	  
+	}
+
+	public Connection getConnection() {
+		// Setup the connection with the DB
+		try {
+			connect = DriverManager
+					.getConnection("jdbc:mysql://www.papademas.net:3307/tickets?autoReconnect=true&useSSL=false"
+							+ "&user=fp411&password=411");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connect;
+	}
+
+	// CRUD implementation
+
+	public void createTables() {
+		// variables for SQL Query table creations
+		final String createTicketsTable = "CREATE TABLE jpapa_tickets(ticket_id INT AUTO_INCREMENT PRIMARY KEY, ticket_issuer VARCHAR(30), ticket_description VARCHAR(200))";
+		final String createUsersTable = "CREATE TABLE jpapa_users(uid INT AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(30), upass VARCHAR(30), admin int)";
+
+		try {
+
+			// execute queries to create tables
+
+			statement = getConnection().createStatement();
+
+			statement.executeUpdate(createTicketsTable);
+			statement.executeUpdate(createUsersTable);
+			System.out.println("Created tables in given database...");
+
+			// end create table
+			// close connection/statement object
+			statement.close();
+			connect.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		// add users to user table
+		addUsers();
+	}
+
+	public void addUsers() {
+		// add list of users from userlist.csv file to users table
+
+		// variables for SQL Query inserts
+		String sql;
+
+		Statement statement;
+		BufferedReader br;
+		List<List<String>> array = new ArrayList<>(); // list to hold (rows & cols)
+
+		// read data from file
+		try {
+			br = new BufferedReader(new FileReader(new File("./userlist.csv")));
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				array.add(Arrays.asList(line.split(",")));
+			}
+		} catch (Exception e) {
+			System.out.println("There was a problem loading the file");
+		}
+
+		try {
+
+			// Setup the connection with the DB
+
+			statement = getConnection().createStatement();
+
+			// create loop to grab each array index containing a list of values
+			// and PASS (insert) that data into your User table
+			for (List<String> rowData : array) {
+
+				sql = "insert into jpapa_users(uname,upass,admin) " + "values('" + rowData.get(0) + "'," + " '"
+						+ rowData.get(1) + "','" + rowData.get(2) + "');";
+				statement.executeUpdate(sql);
+			}
+			System.out.println("Inserts completed in the given database...");
+
+			// close statement object
+			statement.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public int insertRecords(String ticketName, String ticketDesc) {
+		int id = 0;
+		try {
+			statement = getConnection().createStatement();
+			statement.executeUpdate("Insert into jpapa_tickets" + "(ticket_issuer, ticket_description) values(" + " '"
+					+ ticketName + "','" + ticketDesc + "')", Statement.RETURN_GENERATED_KEYS);
+
+			// retrieve ticket id number newly auto generated upon record insertion
+			ResultSet resultSet = null;
+			resultSet = statement.getGeneratedKeys();
+			if (resultSet.next()) {
+				// retrieve first field in table
+				id = resultSet.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return id;
+
+	}
+
+	public ResultSet readRecords() {
+
+		ResultSet results = null;
+		try {
+			statement = connect.createStatement();
+			results = statement.executeQuery("SELECT * FROM jpapa_tickets");
+			//connect.close();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return results;
+	}
+	// continue coding for updateRecords implementation
+
+	// continue coding for deleteRecords implementation
+}
---
+++ src/javaapplication1/Tickets.java
@@ -0,0 +1,155 @@
+package javaapplication1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+
+@SuppressWarnings("serial")
+public class Tickets extends JFrame implements ActionListener {
+
+	// class level member objects
+	Dao dao = new Dao(); // for CRUD operations
+	Boolean chkIfAdmin = null;
+
+	// Main menu object items
+	private JMenu mnuFile = new JMenu("File");
+	private JMenu mnuAdmin = new JMenu("Admin");
+	private JMenu mnuTickets = new JMenu("Tickets");
+
+	// Sub menu item objects for all Main menu item objects
+	JMenuItem mnuItemExit;
+	JMenuItem mnuItemUpdate;
+	JMenuItem mnuItemDelete;
+	JMenuItem mnuItemOpenTicket;
+	JMenuItem mnuItemViewTicket;
+
+	public Tickets(Boolean isAdmin) {
+
+		chkIfAdmin = isAdmin;
+		createMenu();
+		prepareGUI();
+
+	}
+
+	private void createMenu() {
+
+		/* Initialize sub menu items **************************************/
+
+		// initialize sub menu item for File main menu
+		mnuItemExit = new JMenuItem("Exit");
+		// add to File main menu item
+		mnuFile.add(mnuItemExit);
+
+		// initialize first sub menu items for Admin main menu
+		mnuItemUpdate = new JMenuItem("Update Ticket");
+		// add to Admin main menu item
+		mnuAdmin.add(mnuItemUpdate);
+
+		// initialize second sub menu items for Admin main menu
+		mnuItemDelete = new JMenuItem("Delete Ticket");
+		// add to Admin main menu item
+		mnuAdmin.add(mnuItemDelete);
+
+		// initialize first sub menu item for Tickets main menu
+		mnuItemOpenTicket = new JMenuItem("Open Ticket");
+		// add to Ticket Main menu item
+		mnuTickets.add(mnuItemOpenTicket);
+
+		// initialize second sub menu item for Tickets main menu
+		mnuItemViewTicket = new JMenuItem("View Ticket");
+		// add to Ticket Main menu item
+		mnuTickets.add(mnuItemViewTicket);
+
+		// initialize any more desired sub menu items below
+
+		/* Add action listeners for each desired menu item *************/
+		mnuItemExit.addActionListener(this);
+		mnuItemUpdate.addActionListener(this);
+		mnuItemDelete.addActionListener(this);
+		mnuItemOpenTicket.addActionListener(this);
+		mnuItemViewTicket.addActionListener(this);
+
+		 /*
+		  * continue implementing any other desired sub menu items (like 
+		  * for update and delete sub menus for example) with similar 
+		  * syntax & logic as shown above
+		 */
+
+
+	}
+
+	private void prepareGUI() {
+
+		// create JMenu bar
+		JMenuBar bar = new JMenuBar();
+		bar.add(mnuFile); // add main menu items in order, to JMenuBar
+		bar.add(mnuAdmin);
+		bar.add(mnuTickets);
+		// add menu bar components to frame
+		setJMenuBar(bar);
+
+		addWindowListener(new WindowAdapter() {
+			// define a window close operation
+			public void windowClosing(WindowEvent wE) {
+				System.exit(0);
+			}
+		});
+		// set frame options
+		setSize(400, 400);
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// implement actions for sub menu items
+		if (e.getSource() == mnuItemExit) {
+			System.exit(0);
+		} else if (e.getSource() == mnuItemOpenTicket) {
+
+			// get ticket information
+			String ticketName = JOptionPane.showInputDialog(null, "Enter your name");
+			String ticketDesc = JOptionPane.showInputDialog(null, "Enter a ticket description");
+
+			// insert ticket information to database
+
+			int id = dao.insertRecords(ticketName, ticketDesc);
+
+			// display results if successful or not to console / dialog box
+			if (id != 0) {
+				System.out.println("Ticket ID : " + id + " created successfully!!!");
+				JOptionPane.showMessageDialog(null, "Ticket id: " + id + " created");
+			} else
+				System.out.println("Ticket cannot be created!!!");
+		}
+
+		else if (e.getSource() == mnuItemViewTicket) {
+
+			// retrieve all tickets details for viewing in JTable
+			try {
+
+				// Use JTable built in functionality to build a table model and
+				// display the table model off your result set!!!
+				JTable jt = new JTable(ticketsJTable.buildTableModel(dao.readRecords()));
+				jt.setBounds(30, 40, 200, 400);
+				JScrollPane sp = new JScrollPane(jt);
+				add(sp);
+				setVisible(true); // refreshes or repaints frame on screen
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		/*
+		 * continue implementing any other desired sub menu items (like for update and
+		 * delete sub menus for example) with similar syntax & logic as shown above
+		 */
+
+	}
+
+}
